41e346ffac5bb6900d79297fc90e77347907727e
Kamil Grzegorczyk <kamilgrzegorczyk@gmail.com>
2018-09-12 16:11:22 +0200

    Fix ACF Json sync functionality

--- public/core/mu-plugins/raccoon-framework/helpers/acf-json.php
+++ public/core/mu-plugins/raccoon-framework/helpers/acf-json.php
@@ -15,19 +15,19 @@ Class AcfJsonHelper
     {
         if (file_exists($path)) {
             self::$path = $path;
-            add_filter('acf/settings/save_json', ['AcfJsonHelper', 'set_acf_json_save_path']);
-            add_filter('acf/settings/load_json', ['AcfJsonHelper', 'set_acf_json_load_path']);
+            add_filter('acf/settings/save_json', [ __NAMESPACE__  . '\\AcfJsonHelper', 'set_acf_json_save_path']);
+            add_filter('acf/settings/load_json', [ __NAMESPACE__  . '\\AcfJsonHelper', 'set_acf_json_load_path']);
         } else {
             error_log('ACF Json directory doesn\'t exist ' . $path);
         }
     }
 
-    function set_acf_json_save_path()
+    public static function set_acf_json_save_path()
     {
         return self::$path;
     }
 
-    function set_acf_json_load_path($paths)
+    public static function set_acf_json_load_path($paths)
     {
         unset($paths[ 0 ]);
         // append path
--- public/core/mu-plugins/site-framework/setup/acf-json.php
+++ public/core/mu-plugins/site-framework/setup/acf-json.php
@@ -4,11 +4,11 @@ use RaccoonMUFramework\AcfJsonHelper;
 
 //run below only if you have ACF plugin and want to store acf-json outside of a theme (as it should be in most of the cases)
 //todo: avoid anonymous callback
-add_action('init', function () {
+add_action('plugins_loaded', function () {
     if (! function_exists('is_plugin_active')) {
         include_once(ABSPATH . 'wp-admin/includes/plugin.php');
     }
     if (\is_plugin_active('advanced-custom-fields-pro/acf.php') || \is_plugin_active('advanced-custom-fields/acf.php')) {
-        AcfJsonHelper::setup(__DIR__ . 'acf-json');
+        AcfJsonHelper::setup(dirname(__FILE__, 2) . '/acf-json');
     }
 });
