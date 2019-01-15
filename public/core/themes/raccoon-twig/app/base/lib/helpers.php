fc908b71e3f87767d4aa6c3e177f186e21537ea8
Kamil Grzegorczyk <kamilgrzegorczyk@gmail.com>
2019-01-15 19:35:38 +0100

    Fix PHPDoc issue

--- public/core/themes/raccoon-twig/app/base/lib/helpers.php
+++ public/core/themes/raccoon-twig/app/base/lib/helpers.php
@@ -7,7 +7,7 @@ namespace RaccoonTheme;
 /**
  * Returns the dist (compiled) path for give assets. Its just a wrapper around asset loader class.
  *
- * @param $asset_path path to asset in assets directory ('images/something.png')
+ * @param string $asset_path path to asset in assets directory ('images/something.png')
  * @return string
  */
 function get_asset_path($asset_path)
