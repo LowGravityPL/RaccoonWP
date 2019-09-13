ffae09dd2694dfea01a1a118cc54afd87c70da63
Lucas Grzegorczyk <furai@thd.vg>
2019-09-13 20:49:58 +0200

    Add RecentPosts component and mount Vue App component.

---
+++ public/core/themes/raccoon-twig/assets/scripts/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <section id="vue-example" class="recent-posts-wrapper">
+        <RecentPosts />
+    </section>
+</template>
+
+<script>
+import RecentPosts from './components/vue/RecentPosts.vue'
+
+export default {
+    name: 'vue-example',
+    components: {
+        RecentPosts
+    },
+}
+</script>
--- public/core/themes/raccoon-twig/assets/scripts/app.js
+++ public/core/themes/raccoon-twig/assets/scripts/app.js
@@ -1,5 +1,13 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import sayHello from './components/dummy';
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+new Vue({
+    render: h => h(App),
+}).$mount('#vue-example')
 
 console.log('Hello from the application');
 sayHello();
