d187c04f39eadebfe00fff3797a215a15b0e0254
Kamil Grzegorczyk <kamilgrzegorczyk@gmail.com>
2020-07-03 08:10:20 +0200

    Update NPM dependencies, apply new configuration schema for webpack-copy-plugin

--- public/core/themes/raccoon-twig/build-util/common/plugins.js
+++ public/core/themes/raccoon-twig/build-util/common/plugins.js
@@ -1,34 +1,43 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const Copy                   = require('copy-webpack-plugin');
-const ManifestPlugin         = require('webpack-manifest-plugin');
-const VueLoaderPlugin        = require('vue-loader/lib/plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const Copy                 = require('copy-webpack-plugin');
+const ManifestPlugin       = require('webpack-manifest-plugin');
+const VueLoaderPlugin      = require('vue-loader/lib/plugin');
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const commonPaths = require('./paths');
+const commonPaths     = require('./paths');
+const copyGlobOptions = {
+    dot:    true,
+    ignore: ['**/.gitkeep'],
+};
 
 module.exports = {
     plugins: [
         new CleanWebpackPlugin({
-                dry:     false,
-                verbose: true,
-            }),
-        new Copy([
-            {
-                from: 'images/**/*',
-                to:   `${commonPaths.outputPath}`
-            },
-            {
-                from: 'fonts/**/*',
-                to:   `${commonPaths.outputPath}`
-            },
-            {
-                from: '/other/**/*',
-                to:   `${commonPaths.outputPath}`
-            }
-        ], {
-            copyUnmodified: true,
-            context:        `${commonPaths.sourcePath}`
+            dry:     false,
+            verbose: true,
+        }),
+        new Copy({
+            patterns: [
+                {
+                    from:             'images/**/*',
+                    to:               `${commonPaths.outputPath}`,
+                    globOptions:      copyGlobOptions,
+                    noErrorOnMissing: true,
+                },
+                {
+                    from:             'fonts/**/*',
+                    to:               `${commonPaths.outputPath}`,
+                    globOptions:      copyGlobOptions,
+                    noErrorOnMissing: true,
+                },
+                {
+                    from:             'other/**/*',
+                    to:               `${commonPaths.outputPath}`,
+                    globOptions:      copyGlobOptions,
+                    noErrorOnMissing: true,
+                }
+            ]
         }),
         new ManifestPlugin({
             fileName:   'assets-manifest.json',
