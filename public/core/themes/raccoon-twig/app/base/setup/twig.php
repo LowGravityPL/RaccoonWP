a0b73f65c8e25a521ea344c6743ac704796f1b83
Lucas Grzegorczyk <furai@thd.vg>
2023-11-13 14:33:46 +0100

    Update Timber library and composer dependencies

--- public/core/themes/raccoon-twig/app/base/setup/twig.php
+++ public/core/themes/raccoon-twig/app/base/setup/twig.php
@@ -1,59 +1,37 @@
 <?php
+
 declare(strict_types=1);
 
 namespace RaccoonTheme;
 
+use Timber\Timber;
+
 function add_to_timber_context($data)
 {
     //add default header data to all templates
-    $data[ 'header_data' ][ 'info' ] = LayoutDataHelper::getDefaultHeaderData();
+    $data['header_data']['info'] = LayoutDataHelper::getDefaultHeaderData();
 
     //add menu
-    $data[ 'header_data' ][ 'menu_header' ] = new \Timber\Menu('primary_header');
+    $data['header_data']['menu_header'] = Timber::get_menu('primary_header');
 
     //add default footer data to all templates
-    $data[ 'footer_data' ] = LayoutDataHelper::getDefaultFooterData();
+    $data['footer_data'] = LayoutDataHelper::getDefaultFooterData();
 
     //add sidebar
-    $data[ 'sidebar' ][ 'data' ] = \Timber::get_widgets('sidebar-primary');
+    $data['sidebar']['data'] = Timber::get_widgets('sidebar-primary');
 
     return $data;
 }
 
-function display_no_timber_admin_notice()
-{
-    ?>
-    <div class="error">
-        <p>
-            Raccoon theme relies on Timber plugin to process Twig templates. Please activate it.
-            <a href="<?= esc_url(admin_url('plugins.php')) ?>">
-                <?= esc_url(admin_url('plugins.php')) ?>
-            </a>
-        </p>
-    </div>
-    <?php
-}
-
 function initialize_twig_support()
 {
-    /**
-     * Bail if Timber plugin is not active.
-     */
-    if ( ! class_exists('Timber')) {
+    if (class_exists('Timber\Timber')) {
 
-        add_action('admin_notices', __NAMESPACE__ . '\\display_no_timber_admin_notice');
-
-    } else {
-
-        add_filter('timber_context', __NAMESPACE__ . '\\add_to_timber_context');
+        add_filter('timber/context', __NAMESPACE__ . '\\add_to_timber_context');
 
         //add templates location
-        \Timber::$locations = __DIR__ . '/../../templates';
+        Timber::$locations = __DIR__ . '/../../templates';
     }
 }
 
 initialize_twig_support();
-
-
-
-
